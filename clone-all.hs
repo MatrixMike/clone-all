0055d6c3005be0575db96e96b112c881c9ca1cd0
Mike J Hewitt <mikeh@electroteach.com>
2017-09-12 22:33:36 +1000

    after hindent

--- clone-all.hs
+++ clone-all.hs
@@ -2,41 +2,46 @@
 
 module Main where
 
+import Control.Applicative ((<$>), (<*>))
+import Control.Monad (filterM, liftM)
+import Data.Maybe (fromJust, fromMaybe)
+import Data.Monoid ((<>), mempty)
+import Data.Proxy (Proxy(..))
+import Data.Text (pack, unpack)
+import Data.Vector (toList)
+import GitHub.Data (Repo(..))
+import GitHub.Data.Definitions (Owner(..))
+import GitHub.Data.Name (mkName, untagName)
+import GitHub.Data.Repos (RepoPublicity(..))
+import GitHub.Data.URL (getUrl)
+import GitHub.Endpoints.Repos (userRepos)
 import Prelude
-import System.Directory          (doesDirectoryExist)
-import Control.Monad             (filterM, liftM)
-import System.Exit               (ExitCode(..), exitFailure, exitSuccess)
-import System.Process            (system)
-import Control.Applicative       ((<$>), (<*>))
-import Data.Monoid               (mempty, (<>))
-import Data.Maybe                (fromMaybe,fromJust)
-import GitHub.Data               (Repo(..))
-import GitHub.Data.Definitions   (Owner(..))
-import GitHub.Endpoints.Repos    (userRepos)
-import GitHub.Data.Repos         (RepoPublicity(..))
-import GitHub.Data.Name          (untagName,mkName)
-import GitHub.Data.URL           (getUrl)
-import Data.Text                 (unpack,pack)
-import Data.Vector             (toList)
-import Data.Proxy              (Proxy(..))
+import System.Directory (doesDirectoryExist)
+import System.Exit (ExitCode(..), exitFailure, exitSuccess)
+import System.Process (system)
 
+import qualified Options.Applicative as O
 import qualified Options.Applicative.Builder.Internal as X
-import qualified Options.Applicative                  as O
-
 
 toString_ = unpack . untagName
 
-data Options = Options { directory :: String
-                       , user      :: String
-                       } deriving Show
+data Options = Options
+  { directory :: String
+  , user :: String
+  } deriving (Show)
 
 main :: IO ()
 main = O.execParser (O.info (O.helper <*> options) mempty) >>= start
 
 options :: O.Parser Options
-options = Options
-  <$> defStr "."             ( O.metavar "DIRECTORY"            <> O.help "Directory to clone everything into" )
-  <*> O.strOption            ( O.short 'u' <> O.long "user"     <> O.help "Name of the user to clone the repos of.")
+options =
+  Options <$>
+  defStr
+    "."
+    (O.metavar "DIRECTORY" <> O.help "Directory to clone everything into") <*>
+  O.strOption
+    (O.short 'u' <> O.long "user" <>
+     O.help "Name of the user to clone the repos of.")
 
 defStr :: String -> X.Mod X.ArgumentFields String -> O.Parser String
 defStr a = def a . O.argument O.str
@@ -47,26 +52,38 @@ def a = fmap (fromMaybe a) . O.optional
 start :: Options -> IO ()
 start opts = do
   putStrLn $ "Looking up repositories for " ++ (user opts) ++ " ..."
-
-  d <- fmap (fmap toList)  $ userRepos (mkName (Proxy :: Proxy Owner) $ pack $ user opts) RepoPublicityAll
-
+  d <-
+    fmap (fmap toList) $
+    userRepos
+      (mkName (Proxy :: Proxy Owner) $ pack $ user opts)
+      RepoPublicityAll
   case d of
-    Left  err   -> putStrLn $ show err
+    Left err -> putStrLn $ show err
     Right repos -> cloneRepos repos opts
-
 
 cloneRepos :: [Repo] -> Options -> IO ()
 cloneRepos rawRepos opts = do
-  putStrLn $ "Found " ++ (show $ length rawRepos) ++ " repositories for " ++ (user opts) ++ "."
-
+  putStrLn $
+    "Found " ++
+    (show $ length rawRepos) ++ " repositories for " ++ (user opts) ++ "."
   -- Filter the ones that already exist
-  repos <- filterM (\r -> (liftM not) $ doesDirectoryExist $ ((directory opts) ++ "/" ++ ( toString_  $  repoName r))) rawRepos
-
+  repos <-
+    filterM
+      (\r ->
+         (liftM not) $
+         doesDirectoryExist $
+         ((directory opts) ++ "/" ++ (toString_ $ repoName r)))
+      rawRepos
   putStrLn $ (show $ length repos) ++ " that you don't already have."
-
-  returnCodes <- mapM (\r -> system $ "cd " ++ (directory opts) ++ " && git clone " ++ ( unpack $ getUrl $ fromJust $ repoSshUrl r)) repos
+  returnCodes <-
+    mapM
+      (\r ->
+         system $
+         "cd " ++
+         (directory opts) ++
+         " && git clone " ++ (unpack $ getUrl $ fromJust $ repoSshUrl r))
+      repos
   let rs = all (\e -> e == ExitSuccess) returnCodes
-  
-  case rs
-    of True   -> exitSuccess
-       False  -> exitFailure
+  case rs of
+    True -> exitSuccess
+    False -> exitFailure
